19354c19ba36e2a6d49f83d6fb26e1e420770548
Yukari0201 <130890393+Yukari0201@users.noreply.github.com>
2025-10-25 19:21:29 +0800

    init pkg-yuanshen branch

--- utils/build.sh
+++ utils/build.sh
@@ -8,19 +8,19 @@ uv python install 3.12
 uv venv --python 3.12
 source .venv/bin/activate
 uv pip install 'mw2fcitx[opencc]'
-mw2fcitx -c utils/moegirl_dict.py
+mw2fcitx -c utils/yuanshen_dict.py
 
 useradd archbuild
 chmod -R a+rwx .
 export DATE=${1:-$(date +%Y%m%d)} # makepkg.sh also uses the variable
-sed -i "s/0.0.1/${DATE}/" utils/moegirl_dict.py
+sed -i "s/0.0.1/${DATE}/" utils/yuanshen_dict.py
 # https://github.com/docker/for-mac/issues/7331#issuecomment-2198313216
 sudo pacman -U --noconfirm https://archive.archlinux.org/packages/f/fakeroot/fakeroot-1.34-1-x86_64.pkg.tar.zst
 su archbuild utils/makepkg.sh
 
-cp fcitx5-pinyin-moegirl* /artifacts
-cp ./moegirl.dict /artifacts
-cp ./moegirl.dict.yaml /artifacts
+cp fcitx5-pinyin-yuanshen* /artifacts
+cp ./yuanshen.dict /artifacts
+cp ./yuanshen.dict.yaml /artifacts
 cp ./titles.txt /artifacts
 
 popd
---
+++ utils/yuanshen_dict.py
@@ -0,0 +1,34 @@
+from mw2fcitx.tweaks.moegirl import tweaks, tweak_opencc_t2s
+
+exports = {
+    "source": {
+        "api_path": "https://wiki.biligame.com/ys/api.php",
+        "file_path": [
+            # "titles.txt",  # use file-based title source for now
+            "extras/yuanshen.txt"
+        ],
+        "kwargs": {
+            "output": "titles.txt"
+        }
+    },
+    "tweaks": tweaks + [tweak_opencc_t2s],
+    "converter": {
+        "use": "opencc",
+        "kwargs": {
+            "fixfile": "fixfile.json"
+        }
+    },
+    "generator": [{
+        "use": "rime",
+        "kwargs": {
+            "name": "yuanshen",
+            "version": "0.0.1",
+            "output": "yuanshen.dict.yaml"
+        }
+    }, {
+        "use": "pinyin",
+        "kwargs": {
+            "output": "yuanshen.dict"
+        }
+    }]
+}
